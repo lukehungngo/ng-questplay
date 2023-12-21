ba7ed2f651d769b7cac4be543dadcb4091c40d77
hungngo <qhung.bm0409@gmail.com>
2023-12-21 14:54:50 +0700

    #yul-basics

--- campaigns/learning-assembly/yul-basics/contracts/Arithmetic.sol
+++ campaigns/learning-assembly/yul-basics/contracts/Arithmetic.sol
@@ -2,57 +2,40 @@
 pragma solidity ^0.8.19;
 
 contract Arithmetic {
-
     /// @dev Returns lhs + rhs.
     /// @dev You do not need to care about overflow.
-    function addition(
-        int256 lhs, 
-        int256 rhs
-    ) external pure returns (int256 result) {
+    function addition(int256 lhs, int256 rhs) external pure returns (int256 result) {
         assembly {
-
+            result := add(lhs, rhs)
         }
     }
 
     /// @notice Returns lhs * rhs.
     /// @dev You do not need to care about overflow.
-    function multiplication(
-        int256 lhs, 
-        int256 rhs
-    ) external pure returns (int256 result) {
+    function multiplication(int256 lhs, int256 rhs) external pure returns (int256 result) {
         assembly {
-
+            result := mul(lhs, rhs)
         }
     }
 
     /// @notice Returns lhs % rhs.
-    function modulo(
-        uint256 lhs, 
-        uint256 rhs
-    ) external pure returns (uint256 result) {
+    function modulo(uint256 lhs, uint256 rhs) external pure returns (uint256 result) {
         assembly {
-
+            result := mod(lhs, rhs)
         }
     }
 
     /// @notice Returns lhs ^ rhs.
-    function power(
-        uint256 lhs, 
-        uint256 rhs
-    ) external pure returns (uint256 result) {
+    function power(uint256 lhs, uint256 rhs) external pure returns (uint256 result) {
         assembly {
-
+            result := exp(lhs, rhs)
         }
     }
 
     /// @notice Returns the signed division lhs / rhs
-    function signedDivision(
-        int256 lhs, 
-        int256 rhs
-    ) external pure returns (int256 result) {
+    function signedDivision(int256 lhs, int256 rhs) external pure returns (int256 result) {
         assembly {
-
+            result := sdiv(lhs, rhs)
         }
     }
-
 }
