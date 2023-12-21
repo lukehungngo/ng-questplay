9856a385ad7aed5e141b81ce5d28791f2e15c822
hungngo <qhung.bm0409@gmail.com>
2023-12-21 15:15:44 +0700

    #yul-basics

--- campaigns/learning-assembly/yul-basics/contracts/Comparisons.sol
+++ campaigns/learning-assembly/yul-basics/contracts/Comparisons.sol
@@ -2,52 +2,38 @@
 pragma solidity ^0.8.19;
 
 contract Comparisons {
-
     /// @notice Returns value == 0.
     function isZero(int256 value) public pure returns (bool isZero_) {
         assembly {
-
+            isZero_ := iszero(value)
         }
     }
 
     /// @notice Returns lhs > rhs.
-    function greaterThan(uint256 lhs, uint256 rhs)
-        public
-        pure
-        returns (bool greater)
-    {
+    function greaterThan(uint256 lhs, uint256 rhs) public pure returns (bool greater) {
         assembly {
-
+            greater := gt(lhs, rhs)
         }
     }
 
     /// @notice Returns lhs < rhs.
-    function signedLowerThan(
-        int256 lhs, 
-        int256 rhs
-    ) public pure returns (bool lower) {
+    function signedLowerThan(int256 lhs, int256 rhs) public pure returns (bool lower) {
         assembly {
-
+            lower := slt(lhs, rhs)
         }
     }
 
     /// @notice Returns true if value < 0 or value == 10, false otherwise.
-    function isNegativeOrEqualTen(
-        int256 value
-    ) public pure returns (bool negativeOrEqualTen) {
+    function isNegativeOrEqualTen(int256 value) public pure returns (bool negativeOrEqualTen) {
         assembly {
-
+            negativeOrEqualTen := and(lt(value, 0), eq(value, 10))
         }
     }
 
     /// @return inRange true if lower <= value <= upper, false otherwise
-    function isInRange(
-        int256 value,
-        int256 lower,
-        int256 upper
-    ) public pure returns (bool inRange) {
+    function isInRange(int256 value, int256 lower, int256 upper) public pure returns (bool inRange) {
         assembly {
-
+            inRange := and(not(lt(value, lower)), not(gt(lower, upper)))
         }
     }
 }
